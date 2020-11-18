dd74f744004ddeceb38991089d6959754c377720
TimA2-boop <atkesont@gmail.com>
2020-11-18 17:06:51 -0500

    Update ASS.h

--- include/ASSFunctions.h
+++ include/ASSFunctions.h
@@ -3,17 +3,3 @@
 
 //Begins Automated Steering System
 void ASSInit();
-
-//Returns a vector representing a 360 degree scan
-std::vector<bool> scan();
-
-//Picks an angle of next path based on scan data parameter
-//Param degreesOfScanSeparation refers to the gap between individual scans
-double findTurnAngle(std::vector<bool> data, double degreesOfScanSeparation);
-
-//Turns the robot to the specified angle
-void turnToAngle(double angle);
-
-//Drives the robot forward until an object is detected in front
-void driveUntilWall();
-
