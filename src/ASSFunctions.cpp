f183a89ad0e6a6043758310091766ec1b839cb27
alexeyGorbunov02 <72618682+alexeyGorbunov02@users.noreply.github.com>
2021-02-14 23:42:04 -0500

    Added changes spoken of in ASSFunctions from the previous commit; i.e. I actually added them to be commited this time.

--- src/ASSFunctions.cpp
+++ src/ASSFunctions.cpp
@@ -10,11 +10,11 @@ const int RobotLengthCM = 1; //someone needs to measure the robot length, use me
 int IntegralOfDistToTarget = 0;
 int PrevDistToTarget;
 
-int DistToTarget(int LA, int LB, int LC, int LD) {
-  //the L arguments are the distances wrt each sensor.
-  int AngleBetweenSensorsAndWall = atan(2*RobotWidthCM / (abs(LA-LD)+abs(LB-LC)));
-  int HallWidthCM = sin(AngleBetweenSensorsAndWall) * ((LA+LB+LC+LD)/2+RobotWidthCM);
-  return 0; //someone needs to calculate the distance from the center of the robot to the center of the hall and return it. You will need hall width, the angle above, one of the L args, and  one robot dimension measurement.
+int DistToTarget(int FL, int FR, int BL, int BR) {
+  //the arguments are the distances wrt each sensor.
+  int AngleBetweenSensorsAndWall = atan(2*RobotWidthCM / (abs(FL-BL)+abs(FR-BR)));
+  int HallWidthCM = sin(AngleBetweenSensorsAndWall) * ((FL+FR+BL+BR)/2+RobotWidthCM);
+  return 0; //someone needs to calculate the displacement from the center of the robot to the center of the hall and return it (lets say right of target is positive). You will need hall width, the angle above, one of the args, and  one robot dimension measurement.
 }
 
 void AdjustMotorSpeedsWithPID(int Dist) {
@@ -25,19 +25,24 @@ void AdjustMotorSpeedsWithPID(int Dist) {
 }
 
 void ASSInit() {
-  //I am treating RangeFinderA as top left, R.F.B as top right, R.F.C as bottom right, R.F.D as bottom left, R.F.E as front facing (think of it as clockwise ABCD starting at top left with finder E between A and B).
-  //Also all these sonars are currently configured to ports A,B,C,D,E respectively which may need to be changed (in src/robot-config.cpp and include/robot-config.h).
-  while(not RangeFinderE.foundObject()) {
-    double LA = RangeFinderA.distance(mm);
-    double LB = RangeFinderB.distance(mm);
-    double LC = RangeFinderC.distance(mm);
-    double LD = RangeFinderD.distance(mm);
+  //I added LeftMotor and RightMotor to the robot config files in place of the previous SmartTurnSomethings. I also added FrontFacingSonar and configured it to port B which may need to be changed.
+  while(true) { 
+    double LengthFLS = FrontLeftSonar.distance(mm);
+    double LengthFRS = FrontRightSonar.distance(mm);
+    double LengthBLS = BackLeftSonar.distance(mm);
+    double LengthBRS = BackRightSonar.distance(mm);
     int Dist = DistToTarget(LA,LB,LC,LD);
 
     AdjustMotorSpeedsWithPID(Dist);
     
-    //The hope with the following is that once the velocities are altered and the spin functions called, the motors will spin at those velocities. If that is not the case, fix it.
-    LeftMotor.spin(fwd);
-    RightMotor.spin(fwd);
+    //The hope with the following member functions is that every time the velocities are altered in AdjustMotorSpeedsWithPID, the motors will spin at those velocities (given the front sonar doesn't detect a wall). 
+    if not FrontFacingSonar.foundObject() {
+      LeftMotor.spin(fwd);
+      RightMotor.spin(fwd);
+    }
+    else {
+      LeftMotor.stop();
+      RightMotor.stop();
+    }
   }
 }
